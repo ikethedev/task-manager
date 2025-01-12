441a85fccd2fe9720f388fe087853f052e8b1825
ikethedev <isaacdpresley@gmail.com>
2025-01-12 14:58:59 -0500

    topbar edit task function has been added

--- Src/components/topBar/topBar.js
+++ Src/components/topBar/topBar.js
@@ -30,16 +30,12 @@ topBarTemplate.innerHTML = `
 <div class="nav__buttons">
   <button class="primary-btn add-task-btn"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z" fill="white" />
-    </svg>
-  </button>
-    <button class="edit-task-btn"> 
-      <svg width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="1.84615" cy="1.84615" r="1.84615" fill="#828FA3" />
-      <circle cx="1.84615" cy="8.00045" r="1.84615" fill="#828FA3" />
-      <circle cx="1.84615" cy="14.1538" r="1.84615" fill="#828FA3" />
-    </svg>
-  </button>
- 
+    </svg></button>
+  <svg width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="1.84615" cy="1.84615" r="1.84615" fill="#828FA3" />
+    <circle cx="1.84615" cy="8.00045" r="1.84615" fill="#828FA3" />
+    <circle cx="1.84615" cy="14.1538" r="1.84615" fill="#828FA3" />
+  </svg>
 </div>
 </nav>
 `;
