f3878ed97c8a1ceada6e546e0366798aaa4d48e7
ikethedev <isaacdpresley@gmail.com>
2025-01-12 14:58:59 -0500

    topbar edit task function has been added

--- Src/components/topBar/topBar.js
+++ Src/components/topBar/topBar.js
@@ -31,11 +31,12 @@ topBarTemplate.innerHTML = `
   <button class="primary-btn add-task-btn"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z" fill="white" />
     </svg></button>
-  <svg class="edit-task-btn" width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="4" height="16" viewBox="0 0 4 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="1.84615" cy="1.84615" r="1.84615" fill="#828FA3" />
     <circle cx="1.84615" cy="8.00045" r="1.84615" fill="#828FA3" />
     <circle cx="1.84615" cy="14.1538" r="1.84615" fill="#828FA3" />
-  </svg>
+  </svg></button>
+ 
 </div>
 </nav>
 `;
@@ -46,20 +47,9 @@ class TopBar {
     this.showSideBar = this.showSideBar.bind(this)
 
     this.rootElement.querySelector("#nav__logo").addEventListener("click", this.showSideBar)
-    this.rootElement.querySelector(".edit-task-btn").addEventListener("click", this.showEditTaskModule)
-    this.rootElement.querySelector(".add-task-btn").addEventListener("click", this.showAddTaskModule)
     
   }
 
-  showEditTaskModule(){
-    const editModule = new EditTaskModule()
-   document.querySelector(".mainPage").appendChild(editModule.render())
-  }
-
-  showAddTaskModule(){
-    const addModule = new AddTaskModule()
-    document.querySelector(".mainPage").appendChild(addModule.render())
-  }
 
  showSideBar(){ 
     const currentState = sideBarState.getSideBarState();
