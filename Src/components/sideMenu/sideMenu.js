5c4ca438e4304153dc7a938d556251737a5cb48f
ikethedev <isaacdpresley@gmail.com>
2025-01-12 22:17:03 -0500

    function have been added to the side menu dark mode toggle switch but add is still in default

--- Src/components/sideMenu/sideMenu.js
+++ Src/components/sideMenu/sideMenu.js
@@ -1,7 +1,8 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'Src/components/sideMenu/sideMenu.css'; 
-
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "Src/components/sideMenu/sideMenu.css";
+// import { createListItem } from "../../utlis/ulits";
+import { sideBarState } from "../../utlis/sideBarState.js";
 
 const sideMenuTemplate = document.createElement("template");
 sideMenuTemplate.innerHTML = `
@@ -25,21 +26,16 @@ sideMenuTemplate.innerHTML = `
   </header>
   <ul class="sidemenu__list">
     <li class="sidemenu__list-item active">
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="list__path active__path" fill-rule="evenodd" clip-rule="evenodd" d="M0.846133 0.846133C0.304363 1.3879 0 2.12271 0 2.88889V13.1111C0 13.8773 0.304363 14.6121 0.846133 15.1538C1.3879 15.6957 2.12271 16 2.88889 16H13.1111C13.8773 16 14.6121 15.6957 15.1538 15.1538C15.6957 14.6121 16 13.8773 16 13.1111V2.88889C16 2.12271 15.6957 1.3879 15.1538 0.846133C14.6121 0.304363 13.8773 0 13.1111 0H2.88889C2.12271 0 1.3879 0.304363 0.846133 0.846133ZM1.33333 13.1111V8.44448H9.77781V14.6667H2.88889C2.03022 14.6667 1.33333 13.9698 1.33333 13.1111ZM9.77781 7.11111V1.33333H2.88889C2.47633 1.33333 2.08067 1.49723 1.78895 1.78895C1.49723 2.08067 1.33333 2.47633 1.33333 2.88889V7.11111H9.77781ZM11.1111 5.77778H14.6667V10.2222H11.1111V5.77778ZM14.6667 11.5555H11.1111V14.6667H13.1111C13.5236 14.6667 13.9194 14.5028 14.2111 14.2111C14.5028 13.9194 14.6667 13.5236 14.6667 13.1111V11.5555ZM14.6667 2.88889V4.44445H11.1111V1.33333H13.1111C13.5236 1.33333 13.9194 1.49723 14.2111 1.78895C14.5028 2.08067 14.6667 2.47633 14.6667 2.88889Z" />
-      </svg>
+    <img  src="Src/assets/boardIcon.svg"/>
       <p>Platform Launch</p>
     </li>
     <li class="sidemenu__list-item">
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="list__path " fill-rule="evenodd" clip-rule="evenodd" d="M0.846133 0.846133C0.304363 1.3879 0 2.12271 0 2.88889V13.1111C0 13.8773 0.304363 14.6121 0.846133 15.1538C1.3879 15.6957 2.12271 16 2.88889 16H13.1111C13.8773 16 14.6121 15.6957 15.1538 15.1538C15.6957 14.6121 16 13.8773 16 13.1111V2.88889C16 2.12271 15.6957 1.3879 15.1538 0.846133C14.6121 0.304363 13.8773 0 13.1111 0H2.88889C2.12271 0 1.3879 0.304363 0.846133 0.846133ZM1.33333 13.1111V8.44448H9.77781V14.6667H2.88889C2.03022 14.6667 1.33333 13.9698 1.33333 13.1111ZM9.77781 7.11111V1.33333H2.88889C2.47633 1.33333 2.08067 1.49723 1.78895 1.78895C1.49723 2.08067 1.33333 2.47633 1.33333 2.88889V7.11111H9.77781ZM11.1111 5.77778H14.6667V10.2222H11.1111V5.77778ZM14.6667 11.5555H11.1111V14.6667H13.1111C13.5236 14.6667 13.9194 14.5028 14.2111 14.2111C14.5028 13.9194 14.6667 13.5236 14.6667 13.1111V11.5555ZM14.6667 2.88889V4.44445H11.1111V1.33333H13.1111C13.5236 1.33333 13.9194 1.49723 14.2111 1.78895C14.5028 2.08067 14.6667 2.47633 14.6667 2.88889Z" />
-      </svg>
+    <img src="Src/assets/boardIcon.svg"/>
       <p>Marketing Plan</p>
     </li>
     <li class="sidemenu__list-item">
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="list__path " fill-rule="evenodd" clip-rule="evenodd" d="M0.846133 0.846133C0.304363 1.3879 0 2.12271 0 2.88889V13.1111C0 13.8773 0.304363 14.6121 0.846133 15.1538C1.3879 15.6957 2.12271 16 2.88889 16H13.1111C13.8773 16 14.6121 15.6957 15.1538 15.1538C15.6957 14.6121 16 13.8773 16 13.1111V2.88889C16 2.12271 15.6957 1.3879 15.1538 0.846133C14.6121 0.304363 13.8773 0 13.1111 0H2.88889C2.12271 0 1.3879 0.304363 0.846133 0.846133ZM1.33333 13.1111V8.44448H9.77781V14.6667H2.88889C2.03022 14.6667 1.33333 13.9698 1.33333 13.1111ZM9.77781 7.11111V1.33333H2.88889C2.47633 1.33333 2.08067 1.49723 1.78895 1.78895C1.49723 2.08067 1.33333 2.47633 1.33333 2.88889V7.11111H9.77781ZM11.1111 5.77778H14.6667V10.2222H11.1111V5.77778ZM14.6667 11.5555H11.1111V14.6667H13.1111C13.5236 14.6667 13.9194 14.5028 14.2111 14.2111C14.5028 13.9194 14.6667 13.5236 14.6667 13.1111V11.5555ZM14.6667 2.88889V4.44445H11.1111V1.33333H13.1111C13.5236 1.33333 13.9194 1.49723 14.2111 1.78895C14.5028 2.08067 14.6667 2.47633 14.6667 2.88889Z" />
-      </svg>
+    <img src="Src/assets/boardIcon.svg"/>
+
       <p>Roadmap</p>
     </li>
     <li class="sidemenu__list-item platform__create-new">
@@ -71,17 +67,66 @@ sideMenuTemplate.innerHTML = `
 </aside>
 `;
 
+class SideMenu {
+  constructor() {
+    this.rootElement = sideMenuTemplate.content.cloneNode(true);
+    this.rootElement
+      .querySelector(".sidemenu__add-item")
+      .addEventListener("click", this.createNewBoard);
+    this.rootElement
+      .querySelector(".sidemenu__toggle-hide")
+      .addEventListener("click", this.hideSideMenu);
+    this.rootElement
+      .querySelector(".sidemenu__list")
+      .addEventListener("click", this.selectActive)  
+    this.rootElement
+    .querySelector(".view__toggle-btn")
+    .addEventListener("click", this.toggleDarkMode)
+  }
 
-class SideMenu{
-    constructor(){
-        this.rootElement = sideMenuTemplate.content.cloneNode(true)
-    }
+  hideSideMenu() {
+    const currentState = sideBarState.getSideBarState();
+    sideBarState.setSideState(!currentState);
 
-    render(){
-      document.head.appendChild(link);
-       return this.rootElement
+    if (currentState) {
+      document.querySelector(".sidemenu").classList.remove("sidemenu__hide");
+    } else {
+      document.querySelector(".sidemenu").classList.add("sidemenu__hide");
     }
+  }
+
+  toggleDarkMode(){
+    const element = document.querySelector(".view__toggle-btn");
+    element.classList.toggle("dark-active");
+    //remove 
+    alert("dark-mode toggled")
+  }
+
+  selectActive(e){
+    if(e.target.classList.contains("sidemenu__add-item")) return
+
+   document.querySelectorAll(".sidemenu__list-item").forEach(listItem => {
+    listItem.classList.remove("active")
+   })
+
+    if(e.target.parentElement.classList.contains("sidemenu__list-item") || e.target.classList.contains("sidemenu__list-item")){
+      const listItem = e.target.closest(".sidemenu__list-item")
+      if(listItem){
+        listItem.classList.add("active")
+      }
+    }
+  }
+
+  createNewBoard() {
+    // const addBoardModule = new addBoard()
+    // document.querySelector("mainPage").appendChild(addBoardModule.render())
+  }
+
+  render() {
+    document.head.appendChild(link);
+    return this.rootElement;
+  }
 }
 
-const sideMenu = new SideMenu()
-export {sideMenu}
+const sideMenu = new SideMenu();
+export { sideMenu };
