a1bd61f5895fb2adab525b4ecbf3da4188c6c42c
ikethedev <isaacdpresley@gmail.com>
2025-01-13 20:11:56 -0500

    create board and createTask functionality added to data.js

--- Src/utlis/appData.js
+++ Src/utlis/appData.js
@@ -1,5 +1,27 @@
 class AppData{
     constructor(){
+        this.boards = []
+    }
+
+    createBoard({title, task = []} = {}){
+        const board = {
+            id: Math.random().toString(36).substr(2, 9),
+            title,
+            task,
+        }
+        this.boards.push(board)
+        console.log(this.boards)
+    }
+
+    createTask(title, taskDescription, subtasks = [], status = "todo"){
+        const task = {
+            id:  Math.random().toString(36).substr(2, 9),
+            title,
+            d, taskDescription,
+            subtasks: subtasks,
+            status: status
+        }
+        return task
     }
 
 }
